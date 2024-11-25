615eee3b58df3c7bddd0889b173028515a128da1
wangfo <wangfo@ouryun.com.cn>
2024-11-25 19:12:00 +0800

    [cmdline] new port

---
+++ ports/cmdline/portfile.cmake
@@ -0,0 +1,14 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO tanakh/cmdline
+    REF e4cd007fb8f0314002d9a5b4d82939106e4144e4
+    SHA512 0d69105d79672daaf0194f15479794ab1b62c4ae270eb56e6664bc65e4cf4ebbc0d5bf76bc92ecea23fb401121165f9e8a79e39136b34ef680444208294ecf60
+    HEAD_REF master
+)
+
+# Handle copyright
+file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/cmdline)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/cmdline/LICENSE ${CURRENT_PACKAGES_DIR}/share/cmdline/copyright)
+
+# Copy the header files
+file(INSTALL ${SOURCE_PATH}/ DESTINATION ${CURRENT_PACKAGES_DIR}/include/cmdline FILES_MATCHING PATTERN "*.h")
