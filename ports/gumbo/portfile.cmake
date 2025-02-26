3af12ae865cc3a76e747f7ccb70e8c603b4bf896
zhangjinfu <zhangjinfu@ouryun.com.cn>
2025-02-26 17:50:55 +0800

    [gumbo] update port gumbo

--- ports/gumbo/portfile.cmake
+++ ports/gumbo/portfile.cmake
@@ -1,11 +1,10 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
-vcpkg_from_github(
-  OUT_SOURCE_PATH SOURCE_PATH
-  REPO google/gumbo-parser
-  REF v0.10.1
-  SHA512  bb1fb55cd07076ab6a9f38dc14db50397dbdca9a04ace4895dfba8b8cbc09038a96e26070c09c75fa929ada2e815affe233c1e2ecd8afe2aba6201647cf277d1
-  HEAD_REF master
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL https://git.sr.ok/cpp-deps/gumbo-parser.git
+    REF 8dc6ca0470097de9e4e1271e69f382c8486c4c81
+    HEAD_REF master
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
