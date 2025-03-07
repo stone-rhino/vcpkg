b80bdac56d4ed37a186f5b3fa12212f7a4bfca9a
zhangjinfu <zhangjinfu@ouryun.com.cn>
2025-03-07 10:13:34 +0800

    [kyotocabinet] update port kyotocabinet

--- ports/kyotocabinet/portfile.cmake
+++ ports/kyotocabinet/portfile.cmake
@@ -1,5 +1,5 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://dbmx.net/kyotocabinet/pkg/kyotocabinet-${VERSION}.tar.gz"
+    URLS "https://file.sr.ok/CICD/cpp/ata/kyotocabinet-${VERSION}.tar.gz"
     FILENAME "kyotocabinet-${VERSION}.tar.gz"
     SHA512 9fe0a92c9a76db5ce06ef4d5a551c05930f2a9c065ab695b030fdaf45692bfe88d91f1b75791f50d0772c699567744cd74f3ef407172874d4bba467989d54328
 )
