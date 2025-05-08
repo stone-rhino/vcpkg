bb5bc20a4a1ada692984233f2b141874d410489e
zhuangjincheng <zhuangjincheng@ouryun.com.cn>
2025-05-08 16:51:46 +0800

    Revert "[openssl] update port openssl"
    This reverts commit 1287454993212205fd0f5bb18cec9e2489a28c55.

--- ports/openssl/portfile.cmake
+++ ports/openssl/portfile.cmake
@@ -7,10 +7,11 @@ if(VCPKG_TARGET_IS_EMSCRIPTEN)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 endif()
 
-vcpkg_from_git(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    URL https://git.sr.ok/cpp-deps/openssl.git
-    REF 98acb6b02839c609ef5b837794e08d906d965335
+    REPO openssl/openssl
+    REF "openssl-${VERSION}"
+    SHA512 d5f78b2e9d7b7b4787c976c4f832b1448bbadf5f9d398a50ef98053f92501768d000aa73673af200568aef4c8a491442ebbee8c43556838f465d4f91dfc2b5ad
     PATCHES
         cmake-config.patch
         command-line-length.patch
